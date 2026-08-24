d08a9f22f0848563398fe0b96e7c2a423185bb97
sarahfran <sarahfrances.thompson@gmail.com>
2026-08-25 11:30:57 +1200

    Create Header component and style adjustments for global and page layouts

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,24 @@
 import "./globals.css";
+import { Inter, Cal_Sans } from "next/font/google";
+
+const inter = Inter({
+  subsets: ['latin'],
+  style: ['normal'],
+  variable: '--font-inter', 
+  display: 'swap'
+});
+
+const calsans = Cal_Sans({
+  subsets: ['latin'],
+  style: ['normal'],
+  weight: ['400'],
+  variable: '--font-calsans',
+  display: 'swap'
+});
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${inter.variable} ${calsans.variable}`}>
       <body>{children}</body>
     </html>
   );
