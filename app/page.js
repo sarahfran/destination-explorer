14adb3e6a6d93932a94f49c6a62184030caebc00
sarahfran <sarahfrances.thompson@gmail.com>
2026-08-25 15:18:18 +1200

    Add Search component and basic input styling

--- app/page.js
+++ app/page.js
@@ -2,11 +2,13 @@ import styles from "./page.module.css";
 import destinations from "../data/destinations.json";
 import Header from "./components/Header";
 import DestinationGrid from "./components/DestinationGrid";
+import Search from "./components/Search";
 
 export default function HomePage() {
   return (
     <main className={styles.main}>
       <Header />
+      <Search />
       <DestinationGrid destinations={destinations}/>
     </main>
   )
