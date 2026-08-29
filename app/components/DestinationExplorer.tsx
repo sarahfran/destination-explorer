a3f85b9d3f1861533620f396dbd43185480a75ac
sarahfran <sarahfrances.thompson@gmail.com>
2026-08-29 14:37:39 +1200

    Clarify logic and Todo notes for WIP InterestFilters

--- app/components/DestinationExplorer.tsx
+++ app/components/DestinationExplorer.tsx
@@ -14,7 +14,10 @@ interface DestinationExplorerProps {
 export default function DestinationExplorer({destinations} : DestinationExplorerProps){
 
   const [searchTerm, setSearchTerm] = useState<string>("");
+  // const [selectedTags, setSelectedTags] = useState<string[]>([]);
+    // InterestFilters state needs to live in parent comp to access destinations data (not in InterestFilters)
 
+  // Todo: add filter logic for checking selectedTags, as well as searchTerm (to check search input AND selected interests)
   const filteredDestinations = destinations.filter((destination) => {
     return (
       destination.city.toLowerCase().includes(searchTerm.toLowerCase()) ||
@@ -27,7 +30,6 @@ export default function DestinationExplorer({destinations} : DestinationExplorer
       <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       <InterestFilters destinations={destinations}/>
       <DestinationGrid destinations={filteredDestinations} />
-      {/* pass through updated selectedTags state in DestinationGrid */}
     </>
   )
 }
