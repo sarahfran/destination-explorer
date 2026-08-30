635f0517f96c5025c299e4d08ca3a61c622bef40
sarahfran <sarahfrances.thompson@gmail.com>
2026-08-31 10:45:03 +1200

    Remove repeated font styles and improve readability

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,9 +2,9 @@ import styles from './Header.module.css';
 
 export default function Header(){
   return (
-    <header className={styles['header-container']}>
-      <h1 className={styles.header}>The world is your oyster</h1>
-      <h2 className={styles.subheader}>Find inspiration for your next adventure</h2>
+    <header className={styles['header']}>
+      <h1>The world is your oyster</h1>
+      <h2>Find inspiration for your next adventure</h2>
     </header>
   );
 }
