6f9b9b9f29752183bc415580a477d305c9576b54
sarahfran <sarahfrances.thompson@gmail.com>
2026-09-16 11:22:59 +1200

    Add accent colour, fix styling of Clear Search in search input

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -9,12 +9,12 @@ export default function SearchBar({ searchTerm, setSearchTerm } : SearchBarPops)
 
   return (
     <section className={styles.search}>
-       <input type="text" value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)} className={styles['search-input']} placeholder="Search by country or city" />
+      <div className={styles['search-bar']}>
+         <input type="text" value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)} className={styles['search-input']} placeholder="Search by country or city" />
          {searchTerm && (
-          <button onClick={() => setSearchTerm("")}>
-            Clear search
-          </button>
-    )}    
+          <button onClick={() => setSearchTerm("")} className={styles.clear}>Clear search</button>
+        )}  
+      </div>  
     </section>
   )
 }
