1ebe3f891c60978ebf357b615cffbc23c81d5188
sarahfran <sarahfrances.thompson@gmail.com>
2026-09-22 14:20:16 +1200

    Fix alignments for interest filters and region dropdown

--- app/components/InterestFilters.tsx
+++ app/components/InterestFilters.tsx
@@ -42,10 +42,10 @@ export default function InterestFilters({ destinations, selectedTags, setSelecte
 
   return (
 
-    <section className={styles['interest-tags']}>
+    <section className={styles['interest-filters']}>
     {/* <div onClick={toggleDropdown} className={`${styles['interests-dropdown']} ${isVisible ? 'dropdown-open' : ''}`}> */}
       {interestTags.map((tag) => (
-        <span onClick={() => toggleTag(tag)} className={`${styles.tag} ${selectedTags.includes(tag) ? styles['tag-selected'] : ''}`} key={tag}>{tag}</span>
+        <span onClick={() => toggleTag(tag)} className={`${styles.filter} ${selectedTags.includes(tag) ? styles['filter-selected'] : ''}`} key={tag}>{tag}</span>
       ))}
     {/* </div>x */}
     </section>
